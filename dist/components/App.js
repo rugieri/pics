d0b8c2916f743da4e1591b182df48ff40f558988
rugieri <rugieri.ronaldo@gmail.com>
2021-11-24 23:28:15 -0300

    add typescript

---
+++ dist/components/App.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const App = () => {
+    return className = "ui container";
+    style = {};
+    {
+        marginTop: '10px';
+    }
+};
+ >
+    />
+    < /div>;
+;
+;
+exports.default = App;
+//# sourceMappingURL=App.js.map
---
+++ dist/components/SearchBar.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+class SearchBar extends react_1.default.Component {
+    render() {
+        return className = "ui segment" >
+            className;
+        "ui form" >
+            className;
+        "field" >
+            Image;
+        Search < /label>
+            < input;
+        type = "text" /  >
+            /div>
+            < /form>
+            < /div>;
+        ;
+    }
+}
+exports.default = SearchBar;
+//# sourceMappingURL=SearchBar.js.map
