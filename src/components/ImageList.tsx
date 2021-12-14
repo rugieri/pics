3ffc504a20facc0fbba19e60c04fd302e0f2a2e9
rugieri <rugieri.ronaldo@gmail.com>
2021-12-14 20:10:02 -0300

    created an image card component

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import './ImageList.css'
+import ImageCard from './ImageCard';
 interface IProps {
   images: IImage[]
 }
 const ImageList: React.FunctionComponent<IProps> = props =>{
   const imageElements = props.images.map(({description, id, urls})=> {
-    return <img alt={description} key={id} src ={urls.regular} />
+    return <ImageCard key={image.id} image ={image} />
   })
   return <div className = "image-list">{imageElements}</div>
   };
